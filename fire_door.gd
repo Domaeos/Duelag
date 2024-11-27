b97cbcce4b6382b7c8f1617b932f1c98fc814fa0
Ryan Murphy <ryanmurphy.ic@icloud.com>
2024-11-27 17:58:00 +0000

    Overlap WIP

--- fire_door.gd
+++ fire_door.gd
@@ -1,13 +1,15 @@
 extends Door
 
+var door_collision: CollisionShape3D
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	door_area = get_node("Door_B2/Area3D")
+	door_collision = door_area.get_node("CollisionShape3D")
+	door_collision.disabled = false
 	super._ready()
-	pass # Replace with function body.
 
+func _on_overlap_exited(body: Node3D) -> void:
+	door_collision.disabled = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func on_overlap_entered(body: Node3D) -> void:
+	door_collision.disabled = true
