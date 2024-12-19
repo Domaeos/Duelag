53cc99bf185eaf24c8ab78c601c0a38bdda05424
Ryan Murphy <ryanmurphy.ic@icloud.com>
2024-12-19 11:45:06 +0000

    button/gui for res added

--- death_menu.gd
+++ death_menu.gd
@@ -1,15 +1,38 @@
 extends CanvasLayer
 
 @onready var backdrop = $Backdrop
+@onready var res_button = $Control/ResButton
+@onready var res_button_label = $Label
+var resurrect_timer: Timer
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	hide()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+	resurrect_timer = Timer.new()
+	resurrect_timer.wait_time = 3
+	resurrect_timer.one_shot = true
+	resurrect_timer.connect("timeout", _on_resurrect_timeout)
+	add_child(resurrect_timer)
+	
+func _on_resurrect_timeout():
 	pass
+
+func _process(_delta: float) -> void:
+	if resurrect_timer.is_stopped():
+		res_button.disabled = false
+		res_button_label.text = "Resurrect"
+	else:
+		res_button_label.text = str(ceil(resurrect_timer.time_left))
+
 
 func show_menu():
 	show()
+	res_button.disabled = true
+	resurrect_timer.start()
 	backdrop.start_death_overlay()
 	pass
+
+
+func _on_res_button_pressed() -> void:
+	print("Resurrecting!")
+	pass # Replace with function body.
--- door_wood_indoor.gd
+++ door_wood_indoor.gd
@@ -6,8 +6,3 @@ func _ready() -> void:
 	door_area = get_node("Door_A2/Area3D")
 	super._ready()
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
