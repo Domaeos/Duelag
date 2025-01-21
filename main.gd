17472c55c1ed2a0f972f7728019cb7213c858aff
Ryan Murphy <ryanmurphy.ic@icloud.com>
2025-01-21 19:09:29 +0000

    export changes

--- main.gd
+++ main.gd
@@ -4,9 +4,9 @@ var arguments = {}
 const PORT = 9999
 
 var peer = ENetMultiplayerPeer.new()
+var args = OS.get_cmdline_args()
 
 func _ready() -> void:
-	var args = OS.get_cmdline_args()
 	for arg in args:
 		if arg.find("=") > -1:
 			var key_value = arg.split("=")
@@ -20,7 +20,7 @@ func _ready() -> void:
 func _setup_client():
 	var result = peer.create_client("127.0.0.1", PORT)
 	if result != OK:
-		print("Failed to create client connection: ", result)
+		print_verbose("Failed to create client connection: ", result)
 		return
 	
 	multiplayer.multiplayer_peer = peer
@@ -28,20 +28,20 @@ func _setup_client():
 func _setup_server():
 	var result = peer.create_server(PORT)
 	if result != OK:
-		print("Failed to create server on port ", PORT)
+		print_verbose("Failed to create server on port ", PORT)
 		return
 	else:
-		print("Server running on port: ", PORT)
+		print_verbose("Server running on port: ", PORT)
 
 	multiplayer.multiplayer_peer = peer
 	multiplayer.peer_connected.connect(on_peer_connected)
 	multiplayer.peer_disconnected.connect(on_peer_disconnected)
 	
 func on_peer_connected(id: int):
-	print("Peer ", id, " has connected to the server")
+	print_verbose("Peer ", id, " has connected to the server")
 
 func on_peer_disconnected(id: int):
-	print("Peer ", id, " has disconnected from the server")
+	print_verbose("Peer ", id, " has disconnected from the server")
 	var character = get_node_or_null(str(id))
 	if character:
 		character.queue_free()
