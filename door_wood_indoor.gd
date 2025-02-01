a1812df46402aff92c00d4eb3dc96660e4b2efbb
Ryan Murphy <ryanmurphy.ic@icloud.com>
2025-02-01 16:04:25 +0000

    Fix door synchroniser

--- door_wood_indoor.gd
+++ door_wood_indoor.gd
@@ -1,10 +1,5 @@
 extends Door
 
-
-var door_collision: CollisionShape3D
-
 func _ready() -> void:
 	super._ready()
-	door_area = get_node_or_null("Area3D")
-	door_collision = door_area.get_node_or_null("CollisionShape3D")
 	door_collision.disabled = false
